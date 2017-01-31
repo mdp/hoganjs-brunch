9a48617ca9cb23d215a8a5550240ef632b6ba66e
Denys Dovhan <email@denysdovhan.com>
2017-01-31 17:56:52 +0200

    Rewrite in js

---
+++ index.js
@@ -0,0 +1,38 @@
+const hogan = require('hogan.js');
+const sysPath = require('path');
+
+class HoganCompiler {
+  constructor(config) {
+    this.config = config;
+  }
+
+  compile(file) {
+    return new Promise((resolve, reject) => {
+      try {
+        const content = hogan.compile(file.data, {asString: true});
+
+        if (this.config.hoganjs) {
+          if (typeof this.config.hoganjs.wrapper === "function") {
+            return resolve(wrapper(path, content));
+          }
+        }
+
+        resolve({
+          data: `module.exports = new Hogan.Template(${content})`,
+        })
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  get include() {
+    return [sysPath.join(__dirname, '..', 'node_modules', 'hogan.js', 'web', 'builds', '2.0.0', 'template-2.0.0.js')];
+  }
+}
+
+HoganCompiler.prototype.brunchPlugin = true;
+HoganCompiler.prototype.type = 'template';
+HoganCompiler.prototype.extension = 'mustache';
+
+module.exports = HoganCompiler;
