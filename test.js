3f55b257bed2ae6efe520e2f5a4eccfaa260b203
Denys Dovhan <email@denysdovhan.com>
2017-01-31 19:31:04 +0200

    Update tests

---
+++ test.js
@@ -0,0 +1,29 @@
+const _eval = require('eval');
+const Hogan = require('hogan.js');
+const expect = require('expect.js');
+const Plugin = require('.');
+
+describe('Plugin', () => {
+  let plugin;
+
+  beforeEach(() => {
+    plugin = new Plugin({});
+  });
+
+  it('should has #compile method', () =>
+    expect(plugin.compile).to.be.a(Function)
+  );
+
+  it('should compile and produce valid result', () => {
+    const content = 'Follow me @{{name}}';
+    const expected = 'Follow me @mdp';
+
+    return plugin.compile({ data: content })
+      .then(file => {
+        const template = _eval(file.data, 'template.mustache', {Hogan})
+          .render({name: 'mdp'});
+
+        expect(template).to.equal(expected);
+      })
+  });
+});
